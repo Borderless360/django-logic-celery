fb768e528273779c5c8f5cf4374a1c46abd8e48f
Emil <emil.balashov.1@gmail.com>
2020-01-15 14:09:44 +0800

    #19 Fix bug with commands Removed descriptors as it was incorrectly implemented

---
+++ django_logic_celery/transitions.py
@@ -0,0 +1,18 @@
+from django_logic import Transition
+
+from django_logic_celery import SideEffectTasks, CallbacksTasks
+
+
+class InProgressTransition(Transition):
+    side_effects = SideEffectTasks()
+    failure_callbacks = CallbacksTasks()
+
+
+class CeleryCallbackTransition(Transition):
+    callbacks = CallbacksTasks()
+
+
+class CeleryTransition(Transition):
+    side_effects = SideEffectTasks()
+    callbacks = CallbacksTasks()
+    failure_callbacks = CallbacksTasks()
