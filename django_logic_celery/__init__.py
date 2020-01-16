01dd20c13c5a94b1180fce003852df3c70b7db4a
Kirill <khamenman@gmail.com>
2020-01-16 20:18:47 +0400

    bugfix

--- django_logic_celery/__init__.py
+++ django_logic_celery/__init__.py
@@ -1,2 +1,2 @@
 from .commands import SideEffectTasks, CallbacksTasks, SideEffectSingleTask, CallbacksSingleTask
-from .transitions import CeleryCallbackTransition, CeleryTransition, InProgressTransition, CeleryCallbackSingleTaskTransition, CelerySingleTaskTransition
+from .transitions import CeleryCallbackTransition, CeleryTransition, InProgressTransition, CelerySingleTaskTransition
