7fab6bdf29174c50657c70e1f61833d9af12f88c
Emil <emil.balashov.1@gmail.com>
2020-01-19 15:28:09 +0800

    bump version to 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="django-logic-celery",
-    version="0.0.2",
+    version="0.0.3",
     author="Emil Balashov",
     author_email="emil@borderless360.com",
     description="Django Logic Celery - background transitions",
